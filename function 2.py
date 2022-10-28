253b0d33ac51ae267bbebaf329b3ff3539814e1c
Rishipraja <100562380+Rishipraja@users.noreply.github.com>
2022-10-28 07:50:23 +0530

    Add files via upload

---
+++ function 2.py
@@ -0,0 +1,15 @@
+def calculater():
+        a=int(input("enter the first num = "))
+        b=int(input("enter the second num ="))
+        c=a+b
+        print("\n Addition value = ",c)
+        c=a-b
+        print("\n subtraction = ",c)
+        c=a*b
+        print("\n multiplaction = ",c)
+        c=a/b
+        print("\n division = ",c)
+        c=a%b
+        print("\n Modulesh value = ",c)
+   
+calculater()
